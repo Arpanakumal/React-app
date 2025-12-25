0ec4eca1a26c0a5d218d3ec13fe73c41d4accd66
ArpanaKumal <kumalarpana@gmail.com>
2025-12-25 18:48:52 +0545

    Added navbar

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,9 +3,11 @@
 // Import images
 import logo from './logo.png';
 import cleaning from './cleaning.webp';
+import searchIcon from './searchIcon.webp';
 
 // Export images
 export {
     logo,
     cleaning,
+    searchIcon,
 };
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,20 +1,30 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
-import {assets, logo} from '../../assets/assets'
+import { logo, searchIcon } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("Home");
     return (
-        <div classname='navbar'>
-            <img src={logo} alt=""  className="logo"/>
+        <div className="navbar">
+            <img src={logo} alt="Logo" className="logo" />
+
             <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Services</li>
-                <li>Blog</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
+                <li onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("Services")} className={menu === "Services" ? "active" : ""}>Services</li>
+                <li onClick={() => setMenu("Blog")} className={menu === "Blog" ? "active" : ""}>Blog</li>
+                <li onClick={() => setMenu("About us")} className={menu === "About us" ? "active" : ""}>About us</li>
+                <li onClick={() => setMenu("Contact us")} className={menu === "Contact us" ? "active" : ""}>Contact us</li>
             </ul>
+
+
+            <div className="navbar-right">
+                <img src={searchIcon} alt="Search" className="search-icon" />
+                <button>Sign In</button>
+            </div>
         </div>
+
     )
 }
 
