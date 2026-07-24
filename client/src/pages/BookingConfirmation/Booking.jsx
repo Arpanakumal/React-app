1d841112d2f57ad2b1df04ca38c92e75ba3c4cc6
Arpana Kumal <kumalarpana@gmail.com>
2026-07-24 12:34:10 +0545

    Fix Booking.css import

--- client/src/pages/BookingConfirmation/Booking.jsx
+++ client/src/pages/BookingConfirmation/Booking.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import './booking.css';
+import './Booking.css';
 import { StoreContext } from '../../context/StoreContext';
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
