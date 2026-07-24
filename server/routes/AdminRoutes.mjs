69ffdffa3638a9a4e97ac193656a00da468a31f2
Arpana Kumal <kumalarpana@gmail.com>
2026-07-24 13:29:28 +0545

    Replaced Url

--- server/routes/AdminRoutes.mjs
+++ server/routes/AdminRoutes.mjs
@@ -1,3 +1,5 @@
+
+
 import express from "express";
 
 import { adminLogin ,
@@ -10,6 +12,7 @@ import { adminLogin ,
 import authAdmin from "../middleware/authAdmin.mjs";
 
 const adminRouter = express.Router();
+
 
 adminRouter.post("/login", adminLogin);
 adminRouter.get("/dashboard", authAdmin, getAdminDashboard);
